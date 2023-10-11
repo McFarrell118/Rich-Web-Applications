9cfdc7f38cad2bf51f373b207f10ab0176abf529
Michael Farrell <michaelfarrell342@hotmail.com>
2023-10-11 18:04:03 +0100

    Adding content from Rich Web Applications folder

--- script.js
+++
@@ -1,9 +0,0 @@
-alert('Welcome to the home page') 
-
-document.addEventListener('DOMContentLoaded', function() {
-  var button = document.querySelector('.btn');
-
-  button.addEventListener('click', function() {
-      alert('Button was clicked!');
-  });
-});
